77cc9af5fd51c6a218c0eb86af5c201ec62998ca
valeval7 <valeria._mg@hotmail.com>
2025-11-28 23:07:46 -0600

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,13 +16,6 @@ class DatabaseSeeder extends Seeder
    */
   public function run(): void
   {
-    // User::factory(10)->create();
-
-    User::factory()->create([
-      'name' => 'Test User',
-      'email' => 'test@example.com',
-    ]);
-
     // Agregar categorías
     Category::create(['name' => 'Generica', 'description' => 'Generica']);
     Category::create(['name' => 'Economica', 'description' => 'Económica']);
