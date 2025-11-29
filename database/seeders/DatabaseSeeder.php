901fceba70cc796403637606e758950cc983a3c3
valeval7 <valeria._mg@hotmail.com>
2025-11-28 23:03:55 -0600

    Add FIRST categories to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,30 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+  use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    User::factory()->create([
+      'name' => 'Test User',
+      'email' => 'test@example.com',
+    ]);
+
+    // Agregar categorías
+    Category::create(['name' => 'Generica', 'description' => 'Generica']);
+    Category::create(['name' => 'Economica', 'description' => 'Económica']);
+    Category::create(['name' => 'Premium', 'description' => 'Premium']);
+  }
 }
